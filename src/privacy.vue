e9bb883f3f987798807ee2b270df218b7094eb0d
Aditya <adityagiri01101001@gmail.com>
2023-06-15 00:41:39 +0530

    tte

--- src/privacy.vue
+++ src/privacy.vue
@@ -2,6 +2,7 @@
 
 <template>
     <div class="container">
+        <br>
         <h1>Terms and Privacy Policy</h1>
         <p>At dysonos.tech, accessible from https://dysonos.tech/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by dysonos.tech and how we use it. <br>
 
@@ -16,20 +17,6 @@ The personal information that you are asked to provide, and the reasons why you 
 
 If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.<br>
 
-When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number. <br>
-
-<dt>How we use your information</dt><br>
-We use the information we collect in various ways, including to:<br>
-
-Provide, operate, and maintain our website<br>
-Improve, personalize, and expand our website<br>
-Understand and analyze how you use our website<br>
-Develop new products, services, features, and functionality<br>
-Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes<br>
-Send you emails<br>
-Find and prevent fraud <br>
-Log Files<br>
-dysonos.tech follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.<br>
 
 <dt>Cookies and Web Beacons</dt> <br>
 Like any other website, dysonos.tech uses "cookies". These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.<br>
@@ -86,7 +73,7 @@ dysonos.tech does not knowingly collect any Personal Identifiable Information fr
 We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page. <br>
 
 <dt>Contact Us</dt> <br>
-If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us. <br></p>
+If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us. You can contact me at <dt>adityagiri01101001@gmail.com</dt><br></p>
 </div>
 </template>
 
